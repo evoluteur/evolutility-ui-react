45181623280a0f6989339ea1189674480b141e9d
Olivier Giulieri <ogiulieri@gmail.com>
2016-07-06 23:39:17 -0700

    Added delete + i18n file.

---
+++ js/utils/i18n-en.js
@@ -0,0 +1,238 @@
+﻿//   Evolutility Localization Library ENGLISH
+//   (c) 2016 Olivier Giulieri
+
+module.exports = {
+
+	LOCALE: 'EN',    // ENGLISH
+/*
+    getLabel: function(label, string1, string2){
+        var l;
+        if(label.indexOf('.')>-1){
+            var ns=label.split('.');
+            l=this[ns[0]][ns[1]];
+        }else{
+            l=this[label];
+        }
+        if(string1 && l){
+            l = l.replace('{0}', string1);
+            //l = l.replace(/\{0\}/g, string1);
+            if(string2){
+                l = l.replace('{1}', string2);
+                //l = l.replace(/\{1\}/g, string2);
+            }
+        }
+        return l;
+    },
+*/
+    // --- toolbar & buttons ---
+    tools:{
+        View: 'View',
+        bBrowse: 'Browse',
+        bEdit: 'Edit',
+        //bMini: 'Mini', // 'Quick Edit'
+        // Login: 'Login',
+        bNew: 'New',
+        newEntity: 'New {0}', //'New Item',
+        //NewUpload: 'New Upload',
+        //Search: 'Search',
+        //AdvSearch: 'Advanced Search',
+        //NewSearch: 'New Search',
+        //Selections: 'Selections',
+        //Selection: 'Selection',
+        bExport: 'Export',
+        //bImport: 'Import',
+        //SearchRes: 'Search Result',
+        //MassUpdate: 'Mass Update',
+        bDelete: 'Delete',
+        //bAll: 'All',
+        bList: 'List',
+        bCards: 'Cards',
+        bJSON: 'JSON',
+        bFilter: 'Filter',
+        //bBubbles: 'Bubbles',
+        //bSunburst: 'Sunburst',
+        //bScatter:'Scatter',
+        bCharts: 'Charts',
+        //bRefresh: 'Refresh',
+        //bPrint: 'Print',
+        bSave: 'Save',
+        bSaveAdd: 'Save and Add Another',
+        bOK: 'OK',
+        bCancel: 'Cancel',
+
+        // --- data visualization ---
+        vizGroupBy: 'Group by',
+        vizColorBy: 'Color by',
+        vizSizeBy: 'Size by',
+
+        xAxis: 'X Axis',
+        yAxis: 'Y Axis',
+        //zAxis: 'Z Axis',
+
+        // --- wizard ---
+        prev: 'Previous',
+        next: 'Next',
+        finish: 'Finish !'
+    },
+/*
+    // --- msg & status ---
+    //msg: {
+    saved: '{0} saved.',
+    unSavedTitle: 'Changes pending',
+    unSavedChanges: 'Do you want to save the changes you made to "{0}"?',
+    warnNoSave: 'Your changes will be lost if you don\'t save them.',
+    bNoSave: 'Don\'t Save',
+    deleteX: 'Delete {0}',// {0}=entity
+    delete1: 'Do you really want to delete the {0} "{1}"?', // {0}=entity {1}=titlefield value,
+    deleteN: 'Delete {0} {1}?', // delete 5 tasks
+    deleted1: '{0} deleted.', // {0}=entity ,
+
+    notFound: 'Item not found.',
+    //this.setMessage(i18n.notFound, i18n.getLabel('notFoundMsg', this.uiModel.name);
+    notFoundMsg: 'No {0} found.',
+    notFoundMsgId: 'No {0} found for ID="{1}".',
+
+    //NoChange: 'No Change',
+    //NoX: 'No {0}',
+    //Back2SearchResults: 'Back to search results',
+    yes: 'Yes',
+    no: 'No',
+    none: 'None',
+    na: 'N/A', // 'not available'
+    nodata: 'No data available.',
+    notEnoughdata: 'Not enough data available.',
+    nopix: 'No picture.',
+    nochart: 'No charts available.',
+    badchart: 'Not enough information provided to draw charts.',
+    range: '{0} - {1} of {2} {3}', //rangeBegin, '-', rangeEnd, ' of ', mSize, ' ', entities'
+    selected: '{0} selected',
+    //},
+    
+    // --- status ---
+    msg:{
+        sgn_money: '$', // indicator for money
+        sgn_email: '@', // indicator for email
+        added: 'New {0} "{1}" added.',
+        updated: '{0} "{1}" updated.',
+        deleted: '{0} "{1}" deleted.'
+        //error: 'Error',
+    },
+
+    // --- validation ---
+    validation:{
+        incomplete: 'Some information is missing or invalid.',
+        invalid: 'Invalid format.',
+        invalidList: '{0} values in "{1}" are invalid.',
+        invalidList1: '1 value in "{1}" is invalid.',
+        //intro: 'You are not finished yet: ',
+        empty: '"{0}" must have a value.',
+        email: '"{0}" must be a valid email formatted like "name@domain.com".',
+        integer: '"{0}" must only use numbers.',
+        decimal: '"{0}" must be a valid decimal numbers.',
+        money: '"{0}" must be a valid number.',
+        date: '"{0}" must be a valid date, format must be "MM/DD/YYYY" like "12/24/2015".',
+        datetime: '"{0}" must be a valid date/time, format must be "MM/DD/YYYY hh:mm AM/PM" like "12/24/2015 10:30 AM".',
+        time: '"{0}" must be a valid date/time, format must be "hh:mm AM/PM" like "10:30 AM".',
+        json: '"{0}" must be a valid JSON expression like "{"a": 1}".',
+        max: '"{0}" must be smaller or equal to {1}.',
+        min: '"{0}" must be greater or equal to {1}.',
+        maxLength: '"{0}" must be {1} characters long maximum.',
+        minLength: '"{0}" must be at least {1} characters long.',
+        minMaxLength: '"{0}" must be between {1} and {2} characters long.',
+        regExp: '"{0}" is not of the expected format.'
+        //regExp: '"{0}" must match the regular expression pattern for "{1}".'
+    },
+
+    // --- charts ---
+    charts:{
+        aByB: '{0} by {1}',
+        aB: '{0}: {1}'
+    },
+
+    // --- export ---
+    export:{
+        exportOne: 'Export {0}', // {0}=entity
+        exportMany: 'Export {0}', // {0}=entities
+        preview: 'Export preview',
+        header: 'Header',
+        options: 'options',
+        separator: 'Separator',
+        firstLine: 'The first row is a header',
+        format: 'Export format',
+        xpFields: 'Fields to include in the export',
+        IDkey: 'ID',
+        allFields: 'Show all fields',
+        formatCSV: 'Comma separated values (CSV, TXT, XLS...)',
+        formatHTML: 'HTML',
+        formatSQL: 'SQL Insert Statements (SQL)',
+        formatTAB: 'Tab separated values (TXT)',
+        formatXML: 'XML',
+        formatJSON: 'Javascript Object Notation (JSON)',
+        //xpColors: 'Header color-Color odd rows-Color even rows',
+        //xpColMap: 'Columns map to',
+        XMLroot: 'Element name', // 'Root element name'
+        //xpXMLAttr: 'Attributes',
+        //xpXMLElem: 'Elements',
+        headerLabels: 'Field Labels',
+        headerIds: 'Field Attributes or Ids',
+        db: 'Database',
+        SQL: 'SQL Options',
+        SQLTable: 'Table name',
+        SQLTrans: 'In transaction',
+        SQLIdInsert: 'Identity insert',
+        DownloadEntity: 'Download {0}'
+    },
+
+    // --- import ---
+    import:{
+        importOne: 'Import {0}', // {0}=entity
+        importMany: 'Import {0}', // {0}=entities
+        format: 'Source Format',
+        fSample: 'Sample',
+        allowDups: 'Allow duplicates',
+        data: 'Data to Import',
+        success: 'Import done.',
+        empty: 'Nothing to Import.'
+    },
+
+    // --- filters ---
+    filters:{
+        sEqual: 'equals',
+        sNotEqual: 'not equal',
+        sStart: 'starts with',
+        sContain: 'contains',
+        sNotContain: 'doesn\'t contain',
+        sFinish: 'finishes with',
+        sInList: 'is any of',
+        sIsNull: 'is empty',
+        sIsNotNull: 'is not empty',
+        sBefore: 'before',
+        sAfter: 'after',
+        sNumEqual: '&#61;',
+        sNumNotEqual: '!&#61;',
+        sGreater: '&#62;',
+        sSmaller: '&#60;',
+        sOn: 'on',
+        sNotOn: 'not on',
+        sAt: 'at',
+        sNotAt: 'not at',
+        sBetween: 'between',
+        opAnd: 'and',
+        //opOr: 'or',
+        yes: 'Yes',
+        no: 'No',
+        bNewCond: 'New filter condition',
+        bAddCond: 'Add condition',
+        bUpdateFilter: 'Update filter',
+        bSubmit: 'Submit',
+        bCancel: 'Cancel'
+    },
+
+    // --- documentation ---
+    doc:{
+        entity: 'Entity',
+        fields: 'Fields',
+        uiModel: 'UI Model'
+    }*/
+
+};
--- js/widgets/Toolbar.js
+++ js/widgets/Toolbar.js
@@ -1,75 +1,30 @@
-/*
-*/
-var i18nTool = {
-// --- toolbar & buttons ---
-//tools:{
-    View: 'View',
-    bBrowse: 'Browse',
-    bEdit: 'Edit',
-    bMini: 'Mini', // 'Quick Edit'
-    // Login: 'Login',
-    bNew: 'New',
-    newEntity: 'New {0}', //'New Item',
-    //NewUpload: 'New Upload',
-    //Search: 'Search',
-    //AdvSearch: 'Advanced Search',
-    //NewSearch: 'New Search',
-    //Selections: 'Selections',
-    //Selection: 'Selection',
-    bExport: 'Export',
-    bImport: 'Import',
-    //SearchRes: 'Search Result',
-    //MassUpdate: 'Mass Update',
-    bDelete: 'Delete',
-    //bAll: 'All',
-    bList: 'List',
-    bCards: 'Cards',
-    bJSON: 'JSON',
-    bFilter: 'Filter',
-    bBubbles: 'Bubbles',
-    //bSunburst: 'Sunburst',
-    //bScatter:'Scatter',
-    bCharts: 'Charts',
-    //bRefresh: 'Refresh',
-    //bPrint: 'Print',
-    bSave: 'Save',
-    //bSaveAdd: 'Save and Add Another',
-    bOK: 'OK',
-    bCancel: 'Cancel',
 
-    // --- data visualization ---
-    vizGroupBy: 'Group by',
-    vizColorBy: 'Color by',
-    vizSizeBy: 'Size by',
+import React from 'react'
+import $ from 'jquery'
+import { browserHistory } from 'react-router'
+import {apiPath} from '../../config.js'
 
-    xAxis: 'X Axis',
-    yAxis: 'Y Axis',
-    //zAxis: 'Z Axis',
-
-    // --- wizard ---
-    prev: 'Previous',
-    next: 'Next',
-    finish: 'Finish !'
-}
+import NavLink from './NavLink'
+import {tools} from '../utils/i18n-en'
 
 var mm = { 
 
 	always:[
-	    {id: 'list', label: i18nTool.bList, icon:'th-list', n:'x'},
-	    //{id: 'selections', label: i18nTool.Selections, icon:'star', n:'x'},
-	    {id: 'new', label: i18nTool.bNew, icon:'plus', n:'x', readonly:false}
+	    {id: 'list', label: tools.bList, icon:'th-list', n:'x'},
+	    //{id: 'selections', label: tools.Selections, icon:'star', n:'x'},
+	    {id: 'new', label: tools.bNew, icon:'plus', n:'x', readonly:false}
 	],
 	actions:[
-	    //{id:'browse', label: i18nTool.bBrowse, icon:'eye', n:'1', readonly:false},
-	    //{id:'edit', label: i18nTool.bEdit, icon:'edit', n:'1', readonly:false},
-	    //{id:'save', label: i18nTool.bSave, icon:'floppy-disk', n:'1', readonly:false},
-	    {id:'del', label: i18nTool.bDelete, icon:'trash', n:'1', readonly:false}
+	    //{id:'browse', label: tools.bBrowse, icon:'eye', n:'1', readonly:false},
+	    //{id:'edit', label: tools.bEdit, icon:'edit', n:'1', readonly:false},
+	    //{id:'save', label: tools.bSave, icon:'floppy-disk', n:'1', readonly:false},
+	    {id:'del', label: tools.bDelete, icon:'trash', n:'1', readonly:false}
 	],
 	moreActions:[
-	    {id:'filter', label: i18nTool.bFilter, icon:'filter', n:'n'},
+	    {id:'filter', label: tools.bFilter, icon:'filter', n:'n'},
 	    {id:'-'},
-	    {id:'export', label: i18nTool.bExport, icon:'cloud-download',n:'x'},
-	    {id:'import', label: i18nTool.bImport, icon:'cloud-upload',n:'x'},
+	    {id:'export', label: tools.bExport, icon:'cloud-download',n:'x'},
+	    {id:'import', label: tools.bImport, icon:'cloud-upload',n:'x'},
 	    //{id:'-'},
 	    //{id:'cog',label: 'Customize', icon:'cog',n:'x'}
 	],
@@ -79,30 +34,41 @@ var mm = {
 	],
 	views: [
 	    // -- views ONE ---
-	    {id:'browse', label: i18nTool.bBrowse, icon:'eye-open', n:'1'},// // ReadOnly
-	    {id:'edit', label: i18nTool.bEdit, icon:'edit', n:'1', readonly:false},// // All Fields for editing
-	    {id:'mini', label: i18nTool.bMini, icon:'th-large', n:'1', readonly:false},// // Important Fields only
-	    //{id:'wiz',label: i18nTool.bWizard, icon:'arrow-right',n:'1'},
-	    {id:'json', label: i18nTool.bJSON, icon:'barcode', n:'1', readonly:false},
+	    {id:'browse', label: tools.bBrowse, icon:'eye-open', n:'1'},// // ReadOnly
+	    {id:'edit', label: tools.bEdit, icon:'edit', n:'1', readonly:false},// // All Fields for editing
+	    {id:'mini', label: tools.bMini, icon:'th-large', n:'1', readonly:false},// // Important Fields only
+	    //{id:'wiz',label: tools.bWizard, icon:'arrow-right',n:'1'},
+	    {id:'json', label: tools.bJSON, icon:'barcode', n:'1', readonly:false},
 	    // -- views MANY ---
-	    {id:'list', label: i18nTool.bList, icon:'th-list', n:'n'},
-	    {id:'cards', label: i18nTool.bCards, icon:'th-large', n:'n'},
-	    //{id:'bubbles', label: i18nTool.bBubbles, icon:'adjust', n:'n'},
-	    //{id:'scatter', label: i18nTool.bScatter, icon:'certificate',n:'n'},
-	    //{id:'sunburst', label: i18nTool.bSunburst, icon:'record',n:'n'},
-	    {id:'charts', label: i18nTool.bCharts, icon:'stats', n:'n'}
+	    {id:'list', label: tools.bList, icon:'th-list', n:'n'},
+	    {id:'cards', label: tools.bCards, icon:'th-large', n:'n'},
+	    //{id:'bubbles', label: tools.bBubbles, icon:'adjust', n:'n'},
+	    //{id:'scatter', label: tools.bScatter, icon:'certificate',n:'n'},
+	    //{id:'sunburst', label: tools.bSunburst, icon:'record',n:'n'},
+	    {id:'charts', label: tools.bCharts, icon:'stats', n:'n'}
 	],
 	//search: true
 }
 
-import React from 'react'
-import NavLink from './NavLink'
-
 export default React.createClass({
 
-  clickEntity(ent){
-    this.setProps({entity: ent})
-  },
+    deleteOne: function(){
+        //DOTO: SHOULD BE IN STORE BUT THERE IS NO STORE YET
+        var e = this.props.params.entity
+        var id = this.props.params.id || ''
+
+        // TODO: confirmation popup
+        if(id){
+            $.ajax({
+                url: apiPath+e+'/'+id, 
+                type: 'DELETE',
+                success: function (data) {
+                    alert('Item deleted.')
+                    browserHistory.push('/'+e+'/list')
+                }
+            })
+        }
+    },
 
   render() {
     var e=this.props.entity || ''
@@ -112,10 +78,14 @@ export default React.createClass({
         color: 'black',
         backgroundColor: '#eeeeee'
     }
+    var deleteOne=this.deleteOne;
 
     function buttonRoute(m, idx){
-        if(m.id==='save' || m.id==='del'){
+        if(m.id==='save'){
             return <li key={idx}><a href="javascript:void(0)"><i className={'glyphicon glyphicon-'+m.icon}></i> {m.label}</a></li>
+        }
+        if(m.id==='del'){
+            return <li key={idx}><a href="javascript:void(0)" onClick={deleteOne}><i className={'glyphicon glyphicon-'+m.icon}></i> {m.label}</a></li>
         }
         return <li key={idx}><NavLink to={ep+m.id} activeStyle={cStyle}><i className={'glyphicon glyphicon-'+m.icon}></i> {m.label}</NavLink></li>
     } 
