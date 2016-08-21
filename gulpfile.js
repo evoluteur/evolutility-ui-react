547a71e413ab38d4320754631b2656760a8b959c
Olivier Giulieri <ogiulieri@gmail.com>
2016-08-21 11:46:58 -0700

    Changed CSS gulp tasks and header comment.

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,32 @@ var header = require('gulp-header');
 var rename = require('gulp-rename');
 var pkg = require('./package.json');
 
-var banner = '*/ React-Evolutility */';
+var banner = '/**\n  React-Evolutility v<%= pkg.version %>\n\n'+
+            '  <%= pkg.copyright %>\n  <%= pkg.homepage %>\n*/\n';
 
-gulp.task('sass-evol', function () {
+gulp.task('css', function () {
   return gulp.src('./sass/evolutility.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(header(banner, { pkg : pkg }))
     .pipe(gulp.dest('./public'));
 });
-gulp.task('min-evol', function () {
+
+gulp.task('css-min', function () {
   return gulp.src('./sass/evolutility.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(header(banner, { pkg : pkg }))
     .pipe(rename('evolutility.min.css'))
     .pipe(gulp.dest('./public'));
 });
-gulp.task('min-dep', function () {
+
+gulp.task('dep-min', function () {
   return gulp.src('./sass/dependencies.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(rename('dependencies.min.css'))
     .pipe(gulp.dest('./public'));
 });
 
-gulp.task('default', ['sass-evol', 'min-evol', 'min-dep']);
+gulp.task('default', ['css', 'css-min', 'dep-min']);
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
