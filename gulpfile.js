6107bb036841a5fb67381409809340c209620e89
Olivier Giulieri <ogiulieri@gmail.com>
2016-09-11 01:38:13 -0700

    Directory "css" + ASCII art on console when running tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,31 @@ var header = require('gulp-header');
 var rename = require('gulp-rename');
 var pkg = require('./package.json');
 
-var banner = '/**\n  React-Evolutility v<%= pkg.version %>\n\n'+
-            '  <%= pkg.homepage %>\n  <%= pkg.copyright %>\n*/\n';
+var beginBanner = '/**\n  React-Evolutility v<%= pkg.version %>\n\n';
+var banner = beginBanner + 
+    '  <%= pkg.homepage %>\n  <%= pkg.copyright %>\n*/\n';
+var bannerDep = beginBanner + 
+    '  dependencies: Bootstrap (partial), react-datepicker... %>\n*/\n';
+
+var sig='  ___             _\n'+
+' | _ \\___ __ _ __| |_   ___\n'+
+' |   / -_) _` / _|  _| |___| \n'+
+' |_|_\\___\\__,_\\__|\\__| \n'+
+'   ___         _      _   _ _ _ _ \n'+
+'  | __|_ _____| |_  _| |_(_) (_) |_ _  _ \n'+
+'  | _|\\ V / _ \\ | || |  _| | | |  _| || |\n'+
+'  |___|\\_/\\___/_|\\_,_|\\__|_|_|_|\\__|\\_, |\n'+
+'                                    |__/ \n'+
+' Version '+pkg.version+'\n';
+
+console.log(sig);
 
 gulp.task('css', function () {
   return gulp.src('./sass/evolutility.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(rename('react-evolutility.css'))
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('css-min', function () {
@@ -22,17 +38,19 @@ gulp.task('css-min', function () {
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(header(banner, { pkg : pkg }))
     .pipe(rename('react-evolutility.min.css'))
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('dep-min', function () {
   return gulp.src('./sass/dependencies.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(header(bannerDep, { pkg : pkg }))
     .pipe(rename('dependencies.min.css'))
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('default', ['css', 'css-min', 'dep-min']);
+gulp.task('dev', ['css']);
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
