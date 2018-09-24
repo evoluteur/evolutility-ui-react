fc6cf8e375c25cb0ba0f9a639774c80f469cb906
evoluteur <ogiulieri@gmail.com>
2018-09-23 22:25:06 -0700

    Updated url for evolutility-server-node.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 module.exports = {
 
 	// - Path to REST API
-	apiPath: 'http://localhost:3000/api/v1/evolutility/',
+	apiPath: 'http://localhost:2000/api/v1/',
 
 	// - Path to uploaded files
 	filesUrl: 'http://localhost:8080/pix/',
