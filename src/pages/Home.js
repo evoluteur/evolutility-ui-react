4a10d83ed96c35f5f970159bf946df42be23905b
evoluteur <ogiulieri@gmail.com>
2019-01-21 16:32:08 -0800

    Home: moved paragraphs.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,9 +21,27 @@ export default class Home extends React.PureComponent {
             <h1 className="siteTitle"><span style={{fontWeight:'300'}}>Evol</span><span style={{color:'navy'}}>utility</span> <span style={{fontSize: '.5em'}}>v{version}</span> </h1> 
 
             <h2 className="tBlue">Build applications with models rather than code.</h2> 
+            
+            <p><br/>&nbsp;</p>
  
             <div className="component">
-                <h2>Front-end</h2>
+                <h2>Models</h2> 
+
+                <p><a target="db" style={{fontWeight: 600}} href="https://github.com/evoluteur/evolutility-models">Evolutility-Models</a> {' '}
+                are applications definitions covering both back-end (database table and columns...) and front-end (label, width, height...). 
+                </p>
+                <p>
+                    Sample models:{' '}
+                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/todo.js">To-Do list</a>,  {' '}
+                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/contact.js">Address Book</a>,  {' '}
+                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/comics.js">Graphic Novels inventory</a>,  {' '}
+                    <a target="model" href="/https://github.com/evoluteur/evolutility-models/blob/master/models/restaurant.js">Restaurants list</a>, {' '}
+                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/winecellar.js">Wine Cellar</a>.
+                </p>
+           </div>
+
+            <div className="component">
+                <h2>Model-driven front-end</h2>
     
                 <p><a target="ui" style={{fontWeight: 600}} href="https://github.com/evoluteur/evolutility-ui-react">Evolutility-UI-React</a> provides a set of model-driven views to List, Cards, Edit, Browse, and Charts your data. 
                 </p>
@@ -39,7 +57,7 @@ export default class Home extends React.PureComponent {
             </div>
 
             <div className="component">
-                <h2>Back-end</h2>
+                <h2>Model-driven back-end</h2>
                 <p><a target="db" style={{fontWeight: 600}} href="https://github.com/evoluteur/evolutility-server-node">Evolutility-Server-Node</a> provides a set of model-driven REST endpoints for CRUD (Create, Read, Update, Delete) and more.
                 </p> 
                 
@@ -55,21 +73,6 @@ export default class Home extends React.PureComponent {
                 </p> 
            </div> 
            
-           <div className="component">
-                <h2>Models</h2> 
-
-                <p><a target="db" style={{fontWeight: 600}} href="https://github.com/evoluteur/evolutility-models">Evolutility-Models</a> {' '}
-                are applications definitions covering both back-end (database table and columns...) and front-end (label, width, height...). 
-                </p>
-                <p>
-                    Sample models:{' '}
-                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/todo.js">To-Do list</a>,  {' '}
-                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/contact.js">Address Book</a>,  {' '}
-                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/comics.js">Graphic Novels inventory</a>,  {' '}
-                    <a target="model" href="/https://github.com/evoluteur/evolutility-models/blob/master/models/restaurant.js">Restaurants list</a>, {' '}
-                    <a target="model" href="https://github.com/evoluteur/evolutility-models/blob/master/models/winecellar.js">Wine Cellar</a>.
-                </p>
-           </div>
         </div>
         
         );
