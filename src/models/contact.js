87787a7c5eece11d6e53c7810f3f0bdf7a0af20a
evoluteur <ogiulieri@gmail.com>
2019-02-17 18:04:50 -0800

    Changed contact categories.

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,11 +1,13 @@
 var lov_categories = [
-    {id: '1', text: 'Work'},
-    {id: '2', text: 'Fun'},
-    {id: '3', text: 'Travel'},
-    {id: '4', text: 'Business'},
-    {id: '5', text: 'Cars'},
-    {id: '6', text: 'Sport'},
-    {id: '7', text: 'Misc.'}
+    {id: 1, text: 'Friends'},
+    {id: 2, text: 'Family'},
+    {id: 3, text: 'Work'},
+    {id: 4, text: 'Meditation'},
+    {id: 5, text: 'Travel'},
+    {id: 6, text: 'Business'},
+    {id: 7, text: 'Sport'},
+    {id: 8, text: 'Restaurants'},
+    {id: 9, text: 'Misc.'}
 ];
 
 module.exports = {
