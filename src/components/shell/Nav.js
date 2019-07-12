9071b472816bc616818ad5b80492710fc2e30fcb
evoluteur <ogiulieri@gmail.com>
2019-07-11 22:34:24 -0700

    Bug fix in nav bar selection.

--- src/components/shell/Nav.js
+++ src/components/shell/Nav.js
@@ -1,34 +1,61 @@
 import React from 'react';
+import { Route, Link } from "react-router-dom";
 import { i18n_nav } from '../../i18n/i18n';
-import NavSection from './NavSection.js';
 import AppMenus from '../../AppMenus.js';
 
 import './Nav.scss';
 
-// url = http://localhost:2000/api/v1/object?object?active=eq.true
+const vIcons = [
+    {id: '/list', icon:'th-list'},
+    //{id: '/cards', icon:'th-large'},
+    {id: '/charts', icon:'stats'},
+    //{id: '/stats', icon:'equalizer'},
+    {id: '/edit/0', icon:'plus'},
+]
+
+const iconViews = (mid, f) => (
+    <div className="mIcons" >
+        {vIcons.map(menu => f.url ? null : <Link to={'/'+mid+menu.id} key={menu.id}><i className={'glyphicon glyphicon-'+menu.icon}/></Link>)}
+    </div>
+)
+
+const MenuLink = ({ menu }) => <Route
+        path={'/'+menu.id}
+        exact={false}
+        children={({ match }) => (
+            <li className={match ? "active" : ""}>
+            <Link to={'/'+menu.id}>{menu.text}</Link>
+            {iconViews(menu.id, menu)}
+            </li>
+        )}
+    />
+
+const MenuLinks = ({ menus }) => menus.map(menu => <MenuLink menu={menu} key={menu.id} />)
 
 export default class Nav extends React.Component {
 
     render() {
-        const cRoute = window.location.pathname.substring(1, 100)
-        const activeGroup = AppMenus[cRoute] || ''
 
         return (
             <nav className="Nav">
                 <a className="skipNav" href="#afterNav">{i18n_nav.skip}</a>
                 <ul>
-                    {AppMenus.map((g)=>(
-                        <NavSection
-                            route = {cRoute}
-                            active = {activeGroup===g.id}
-                            model = {g}
-                            key = {g.id}
-                        />
+                    {AppMenus.map((section)=>( 
+                        <li className={this.props.active?'active-li':''} key={section.id}>
+                            {section.title ? (
+                                <div>
+                                    <i className={'glyphicon glyphicon-'+section.icon} name={section.icon}/>
+                                        {section.title}
+                                </div>
+                            ) : null}
+                            <ul className="nav-l2">
+                                <MenuLinks menus={section.menus} />
+                            </ul>
+                        </li>
                     ))}
                 </ul>
                 <div id="afterNav"></div>
             </nav>
         );
     }
-
 }
