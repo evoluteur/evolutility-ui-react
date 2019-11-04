145fe2538a9ee922a8530f02ecc3452b78a5b739
evoluteur <ogiulieri@gmail.com>
2019-11-03 20:25:23 -0800

    Cleanups.

--- src/components/pages/Demo.js
+++ src/components/pages/Demo.js
@@ -24,20 +24,20 @@ export default class Demo extends React.PureComponent {
                     <div className="component c1"> 
                         <h3>Personal Organizer</h3>
                         <div className="demoLinks">
-                            <Link to="/todo/list"><img src="/pix/todo.gif" alt="" title="To-Do list"/>To-Do list</Link>
-                            <Link to="/contact/list"><img src="/pix/contact.gif" alt="" title="Address book"/>Address book</Link>
-                            <Link to="/comics/cards"><img src="/pix/comics.png" alt="" title="Graphic Novels inventory"/>Graphic novels</Link>
-                            <Link to="/restaurant/list"><img src="/pix/resto.gif" alt="" title="Restaurants list"/>Restaurants</Link> 
-                            <Link to="/winecellar/list"><img src="/pix/wine-bottle.png" alt="" title="Wine Cellar"/>Wine Cellar</Link>
-                            <Link to="/winetasting/list"><img src="/pix/wine.gif" alt="" title="Wine Tasting"/>Wine Tasting</Link>
+                            <Link to="/todo/list"><img src="/pix/todo.gif" alt="" title="To-Do list"/>To-Do list</Link><br/>
+                            <Link to="/contact/list"><img src="/pix/contact.gif" alt="" title="Address book"/>Address book</Link><br/>
+                            <Link to="/comics/cards"><img src="/pix/comics.png" alt="" title="Graphic Novels inventory"/>Graphic novels</Link><br/>
+                            <Link to="/restaurant/list"><img src="/pix/resto.gif" alt="" title="Restaurants list"/>Restaurants</Link><br/>
+                            <Link to="/winecellar/list"><img src="/pix/wine-bottle.png" alt="" title="Wine Cellar"/>Wine Cellar</Link><br/>
+                            <Link to="/winetasting/list"><img src="/pix/wine.gif" alt="" title="Wine Tasting"/>Wine Tasting</Link><br/>
                         </div>
                     </div>
                     <div className="component"> 
                         <h3>Music</h3>
                         <div className="demoLinks">
-                            <Link to="/artist/cards"><img src="/pix/star.png" alt="" title="Artists"/>Artists</Link>
-                            <Link to="/album/cards"><img src="/pix/cd.png" alt="" title="Albums"/>Albums</Link>
-                            <Link to="/track/list"><img src="/pix/music.png" alt="" title="Music tracks"/>Tracks</Link>
+                            <Link to="/artist/cards"><img src="/pix/star.png" alt="" title="Artists"/>Artists</Link><br/>
+                            <Link to="/album/cards"><img src="/pix/cd.png" alt="" title="Albums"/>Albums</Link><br/>
+                            <Link to="/track/list"><img src="/pix/music.png" alt="" title="Music tracks"/>Tracks</Link><br/>
                         </div>
                     </div>
                 </div>
