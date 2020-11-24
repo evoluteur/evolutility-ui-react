de1494c322089f14d9ec41b03fa2d9b694d2c0e1
evoluteur <ogiulieri@gmail.com>
2020-11-23 20:45:31 -0800

    Cleanup: declare before export.

--- src/components/views/charts/ChartProps.js
+++ src/components/views/charts/ChartProps.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const sizes = ['small', 'large']
 const chartTypes = ['bars', 'pie', 'table']
 
-export default {
+const chartProps = {
     sizes: sizes,
     chartTypes: chartTypes,
     chartProps: {
@@ -11,3 +11,5 @@ export default {
         size:  PropTypes.oneOf(sizes),
     }
 }
+
+export default chartProps
