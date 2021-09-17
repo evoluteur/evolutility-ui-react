b01708449ccb7054362773c34b981a32e17abb43
Olivier Giulieri <ogiulieri@gmail.com>
2021-09-17 11:27:22 -0700

    Smaller footer.

--- src/components/shell/Footer.js
+++ src/components/shell/Footer.js
@@ -8,17 +8,19 @@ export default class Footer extends React.PureComponent {
 
     render() {
         return <div className="Footer" role="contentinfo">
+            <a className="copyright" target="ui" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-ui-react"><span>Evolutility-UI-React v{version}</span></a>
+            <div>&copy; {copyright.replace('(c)', '')}</div>
+        </div>
+    }
+    /*
             <div className="Footer-links">
             	Evolutility{' = '}
-            	<a target="m" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-models">Models</a> 
+            	<a target="m" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-models">Models</a>
                 {' + '}
-            	<a target="ui" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-ui-react">UI</a> 
+            	<a target="ui" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-ui-react">UI</a>
                 {' + '}
             	<a target="db" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-server-node">Server</a>
             </div>
-            <span className="copyright">
-                <span>Evolutility-UI-React v{version}</span> <span>&copy; {copyright.replace('(c)', '')}</span>
-            </span>
-        </div>
-    }
+
+    */
 }
