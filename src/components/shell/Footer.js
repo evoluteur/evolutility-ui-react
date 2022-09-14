540b4445070a5337c584626546928b89b59941df
Olivier Giulieri <ogiulieri@gmail.com>
2022-09-13 18:55:12 -0700

    Cleanups.

--- src/components/shell/Footer.js
+++ src/components/shell/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import packageInfo from "../../../package.json";
 
 import "./Footer.scss";
@@ -22,22 +21,15 @@ const Footer = () => (
     <div></div>
     <div>
       &copy; {copyright.substring(4, 9)}
-      <a href="https://evoluteur.github.io/" target="og">
+      <a
+        href="https://evoluteur.github.io/"
+        target="og"
+        rel="noopener noreferrer"
+      >
         Olivier Giulieri
       </a>
     </div>
   </div>
 );
-/*
-            <div className="Footer-links">
-            	Evolutility{' = '}
-            	<a target="m" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-models">Models</a>
-                {' + '}
-            	<a target="ui" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-ui-react">UI</a>
-                {' + '}
-            	<a target="db" rel="noopener noreferrer" href="https://github.com/evoluteur/evolutility-server-node">Server</a>
-            </div>
-
-    */
 
 export default Footer;
