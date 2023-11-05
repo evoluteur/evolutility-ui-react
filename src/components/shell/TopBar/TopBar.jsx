c13f31e535e0c28255b454aa243900ca3a1548c5
Olivier Giulieri <ogiulieri@gmail.com>
2023-11-05 01:21:49 -0800

    Changed logo in topbar.

--- src/components/shell/TopBar/TopBar.jsx
+++ src/components/shell/TopBar/TopBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Icon from "react-crud-icons";
-import logoEvol from "./evologo.gif";
+import logoEvol from "./evologo.png";
 import { views, modelViewsMany } from "../../../utils/dicoViews";
 import { getModel } from "../../../utils/moMa";
 import ViewActions from "./ViewActions";
