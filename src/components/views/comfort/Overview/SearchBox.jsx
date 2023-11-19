1c850f1be4340ea66ea15f64975d48eeb93a963a
Olivier Giulieri <ogiulieri@gmail.com>
2023-11-19 01:08:10 -0800

    Added Search icon.

--- src/components/views/comfort/Overview/SearchBox.jsx
+++ src/components/views/comfort/Overview/SearchBox.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
-import Typeahead from "../../../Field/edit/FieldObject";
+import Icon from "react-crud-icons";
+import FieldObject from "../../../Field/edit/FieldObject";
+
+import "./SearchBox.scss";
 
 const SearchBox = ({ entity, placeHolder }) => {
+  const [value, setValue] = useState(null);
   const navigate = useNavigate();
-  const handleSearch = (valueObj) => {
-    const id = valueObj?.value;
-    if (id) {
-      navigate(`/${entity}/browse/${id}`);
+  const onChange = useCallback(
+    (valueObj) => {
+      const id = valueObj?.value;
+      if (id) {
+        navigate(`/${entity}/browse/${id}`);
+      }
+    },
+    [entity, navigate]
+  );
+  const onInputChange = useCallback((value) => setValue(value), []);
+  const onClick = useCallback(() => {
+    if (value) {
+      navigate(`/${entity}/list/?search=${value}`);
     }
-  };
+  }, [entity, value, navigate]);
+
   return (
     <div className="evo-search-box">
-      <Typeahead
-        id="search"
-        entity={entity}
-        onChange={handleSearch}
-        placeHolder={placeHolder}
-      />
+      <div>
+        <FieldObject
+          id="search"
+          entity={entity}
+          onChange={onChange}
+          onInputChange={onInputChange}
+          placeHolder={placeHolder}
+        />
+        <button className="btn btn-default" onClick={onClick}>
+          <Icon name="search" theme="none" />
+        </button>
+      </div>
     </div>
   );
 };
