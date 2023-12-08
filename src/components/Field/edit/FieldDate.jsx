85d0cc8869114d433eb01568384ef58a18a5f48d
Olivier Giulieri <ogiulieri@gmail.com>
2023-12-07 17:02:34 -0800

    Simpler way to disable autocomplete on datepicker.

--- src/components/Field/edit/FieldDate.jsx
+++ src/components/Field/edit/FieldDate.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, memo } from "react";
+import React, { useCallback, memo } from "react";
 import PropTypes from "prop-types";
 import Datepicker from "react-datepicker";
 import { trueDate } from "utils/format";
@@ -7,11 +7,6 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./FieldDate.scss";
 
 // TODO: set maxDate and minDate (not urgent if caught w/ validation)
-
-const noSuggestion = (id) => {
-  const elem = document.getElementById(id);
-  elem?.setAttribute("autocomplete", "off");
-};
 
 const FieldDate = memo(({ id, value, onChange }) => {
   const onDateChange = useCallback(
@@ -25,16 +20,13 @@ const FieldDate = memo(({ id, value, onChange }) => {
     [id, onChange]
   );
 
-  useEffect(() => {
-    noSuggestion(id);
-  }, [id]);
-
   return (
     <Datepicker
       id={id}
       selected={trueDate(value)}
       onChange={onDateChange}
       className="form-control"
+      autoComplete="off"
     />
   );
 });
