dc1d0fed663c485ea9dd5eb472a51014e9810cf5
Olivier Giulieri <ogiulieri@gmail.com>
2026-06-15 23:53:22 -0700

    fix: demo links when nested in home page.

--- src/components/views/comfort/ModelLinks.jsx
+++ src/components/views/comfort/ModelLinks.jsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
-import { pixPath } from "utils/format";
+import { evoPath, pixPath } from "utils/format";
 import { modelsArray } from "utils/moMa";
 
 import "./ModelLinks.scss";
@@ -9,7 +9,7 @@ const ModelLinks = memo(() => (
   <div className="evo-models-list">
     {modelsArray?.map((m) => (
       <div key={m.id}>
-        <Link to={`../${m.id}/${m.defaultViewMany || "list"}`}>
+        <Link to={`/${evoPath}/${m.id}/${m.defaultViewMany || "list"}`}>
           <img className="e-icon" src={pixPath + m.icon} alt="" />
           {m.title}
         </Link>
